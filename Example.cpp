d112fb8ec9f4573cb4604ee26625527a95fe9ed9
dehoisted <75084509+dehoisted@users.noreply.github.com>
2021-01-25 20:10:33 -0500

    How to use

--- Example.cpp
+++ Example.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
-#include "Colors.h"
-
 int main()
 {
-    std::string phrase = "Hello!";
-    color.green(phrase);
+    // Parse desired color for 1 line
+    color(2, "I chose a color I wanted for this line!"); // Green
+
+    // Set whole program to desired color
+    set_color(5); // Purple
+    std::cout << "Using set color will change all text to 1 color" << std::endl;
+    std::cout << "That means this text will have color too" << std::endl;
+
+    // Basic Function
+    red("Red is here already so I can do this easily"); // Red
+
+	return 0;
 }
