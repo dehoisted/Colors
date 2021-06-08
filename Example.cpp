06b07118f4efb20bde9355273ceea2ba901efb41
orlando <75084509+dehoisted@users.noreply.github.com>
2021-06-08 19:17:03 -0400

    Update Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -1,16 +1,11 @@
-// Example -- Colors
+#include <iostream>
+#include "Colors.h"
+
 int main()
 {
-    // Parse desired color for 1 line
-    color(2, "I chose a color I wanted for this line!"); // Green
-
-    // Set whole program to desired color
-    set_color(5); // Purple
-    std::cout << "Using set color will change all text to 1 color" << std::endl;
-    std::cout << "That means this text will have color too" << std::endl;
-
-    // Basic Function
-    red("Red is here already so I can do this easily"); // Red
-
-	return 0;
+	Colors::print("hi", 3);
+	Colors::set_color(1);
+	std::cout << "hi 1";
+	Colors::ResetC();
+	Colors::light_blue("hi 2");
 }
